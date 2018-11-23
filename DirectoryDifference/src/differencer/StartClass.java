e15caa1d3f0b79acc4a01d67bec8c39acda7d4a8
Sancho Pansa <zelgadis4@gmail.com>
2018-11-23 17:55:49 +0300

    Main class converted into GUI entry point

---
+++ DirectoryDifference/src/differencer/StartClass.java
@@ -0,0 +1,21 @@
+package differencer;
+
+import java.io.File;
+import java.util.*;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class StartClass extends Application {
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+	}
+
+	public static void main(String[] args) {
+		launch();
+	}
+
+	
+}
